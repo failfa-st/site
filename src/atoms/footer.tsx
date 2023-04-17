bcc75eef2811ef06e5ba438c44ae44a9ea5583fe
TimPietrusky <timpietrusky@gmail.com>
2023-04-18 00:33:45 +0200

    feat: added legal to footer

--- src/atoms/footer.tsx
+++ src/atoms/footer.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/Footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
@@ -6,6 +7,7 @@ const Footer = () => {
       <div className={styles.container}>
         <div className={styles.left}>© 2023 failfast</div>
         <div className={styles.right}>
+          <Link href="/legal/data-policy">Legal</Link>
           <a
             href="https://twitter.com/failfa_st"
             target="_blank"
