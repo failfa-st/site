bcc580064d93efe9d7ce366d11cf7b405657cbe5
Gregor Adams <greg@pixelass.com>
2023-04-20 01:01:04 +0200

    feat: add codesandboxes to gallery

---
+++ src/components/codesandbox-embed.tsx
@@ -0,0 +1,64 @@
+import { useCookieConsentContext } from "@use-cookie-consent/react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { useState } from "react";
+
+export default function CodesandboxEmbed({ src }: { src: string }) {
+	const { consent, acceptCookies } = useCookieConsentContext();
+	const [loaded, setLoaded] = useState(false);
+
+	return (
+		<Box sx={{ position: "relative", paddingBottom: `${(100 / 16) * 9}%` }}>
+			{consent.thirdParty && loaded ? (
+				<Box
+					component="iframe"
+					src={src}
+					allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+					sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+					sx={{
+						height: "100%",
+						width: "100%",
+						position: "absolute",
+						inset: 0,
+						color: "primary.main",
+						border: "1px solid currentColor",
+						borderRadius: 2,
+						overflow: "hidden",
+					}}
+				/>
+			) : (
+				<Box
+					sx={{
+						height: "100%",
+						width: "100%",
+						position: "absolute",
+						inset: 0,
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+						color: "primary.main",
+						border: "1px solid currentColor",
+						borderRadius: 2,
+					}}
+				>
+					{!consent.thirdParty && (
+						<Button
+							onClick={() => {
+								acceptCookies({ ...consent, thirdParty: true });
+							}}
+						>
+							Allow Third Party Cookies
+						</Button>
+					)}
+					<Button
+						onClick={() => {
+							setLoaded(true);
+						}}
+					>
+						Show Sandbox
+					</Button>
+				</Box>
+			)}
+		</Box>
+	);
+}
---
+++ src/pages/gallery.tsx
@@ -0,0 +1,76 @@
+import Layout from "@/components/layout";
+import Grid from "@mui/material/Unstable_Grid2";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import dynamic from "next/dynamic";
+
+const CodesandboxEmbed = dynamic(async () => await import("@/components/codesandbox-embed"), {
+	ssr: false,
+});
+
+export default function Page() {
+	return (
+		<Layout>
+			<Container sx={{ mt: 6 }}>
+				<Typography variant="h1">100% Prompt driven Codesandboxes</Typography>
+				<Typography variant="h2">
+					Welcome to the Future of Coding: &quot;fail4&quot; Generated Codesandboxes!
+				</Typography>
+				<Typography my={2}>
+					Dive into the world of AI-driven coding with &quot;fail4&quot;, our innovative
+					prompt-driven tool that revolutionizes the way you create canvas experiments.
+					Customize your code generation with options like temperature and watch as the AI
+					crafts fully interactive codesandboxes tailored to your needs.
+				</Typography>
+				<Typography my={2}>
+					Explore, experiment, and elevate your coding skills with our diverse collection
+					of &quot;fail4&quot; generated codesandboxes. Embrace the future of AI-powered
+					development and unleash your creativity today!
+				</Typography>
+
+				<Grid container columns={{ xs: 1, md: 2 }} spacing={2} sx={{ mt: 6 }}>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/infinite-running-connected-dots-animation-is-in-darkmode-is-for-codepen-io-oovdz1"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							infinite running &quot;connected dots&quot; animation, is in darkmode,
+							is for codepen.io
+						</Typography>
+					</Grid>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/matrix-code-8rgb8t"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							matrix code
+						</Typography>
+					</Grid>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/cool-click-effect-add-dark-background-write-click-into-the-middle-ss4m9b"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							cool click effect, add dark background, write &quot;click&quot; into the
+							middle
+						</Typography>
+					</Grid>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/rainbow-colored-balls-in-various-sizes-bouncing-on-the-walls-and-colliding-with-each-other-91ydi2"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							rainbow colored balls in various sizes, bouncing on the walls and
+							colliding with each other
+						</Typography>
+					</Grid>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/fancy-visual-effect-for-codepen-io-dark-background-infinite-motion-endless-world-9pbzpr"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							fancy visual effect for codepen.io, dark background, infinite motion,
+							endless world
+						</Typography>
+					</Grid>
+					<Grid xs={1}>
+						<CodesandboxEmbed src="https://codesandbox.io/embed/flow-field-slow-motion-animation-rainbow-colors-dark-background-wsneu3"></CodesandboxEmbed>
+						<Typography mt={2} variant="h4" component="h3">
+							flow field, slow motion, animation, rainbow colors, dark background
+						</Typography>
+					</Grid>
+				</Grid>
+			</Container>
+		</Layout>
+	);
+}
