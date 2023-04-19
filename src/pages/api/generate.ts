c840bc815450e26f39f50ca1e98bf271c2baec04
Tim Pietrusky <timpietrusky@gmail.com>
2023-04-19 19:08:29 +0200

    fix: added NEXTAUTH_SECRET and getToken

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,14 +1,10 @@
 import { toOpenAI } from "@/projects/fail4/services/api";
 import { NextApiRequest, NextApiResponse } from "next";
 import { AxiosError } from "axios";
-import { getServerSession } from "next-auth";
 import { getToken } from "next-auth/jwt";
-import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(request: NextApiRequest, response: NextApiResponse) {
 	const token = await getToken({ req: request });
-
-	console.log(token);
 
 	if (!token) {
 		response.status(401).json({ message: "Unauthorized" });
