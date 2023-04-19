73b54e2e18495b385dde9201533b37354405c5d3
Gregor Adams <greg@pixelass.com>
2023-04-19 19:56:34 +0200

    fix: formatting was faulty

--- src/projects/fail4/utils/index.ts
+++ src/projects/fail4/utils/index.ts
@@ -1,33 +1,32 @@
 import GPT3Tokenizer from "gpt3-tokenizer";
 import prettier from "prettier";
+import parserBabel from "prettier/parser-babel";
 
 export const tokenizer = new GPT3Tokenizer({ type: "gpt3" });
 
 export function getTokens(text: string) {
-  return tokenizer.encode(text).bpe.length;
+	return tokenizer.encode(text).bpe.length;
 }
 
 export function prettify(code: string) {
-  try {
-    return prettier.format(code, {
-      useTabs: true,
-      semi: true,
-      parser: "babel",
-    });
-  } catch {
-    return code;
-  }
+	try {
+		return prettier.format(code, {
+			useTabs: true,
+			semi: true,
+			parser: "babel",
+			plugins: [parserBabel],
+		});
+	} catch (error) {
+		return code;
+	}
 }
 
-export function getTheme(
-  mode: string | undefined,
-  systemMode: string | undefined
-) {
-  if (mode === "system") {
-    return `vs-${systemMode}`;
-  }
-  if (mode) {
-    return `vs-${mode}`;
-  }
-  return undefined;
+export function getTheme(mode: string | undefined, systemMode: string | undefined) {
+	if (mode === "system") {
+		return `vs-${systemMode}`;
+	}
+	if (mode) {
+		return `vs-${mode}`;
+	}
+	return undefined;
 }
