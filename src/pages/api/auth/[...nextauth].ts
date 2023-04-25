7fd4fdeddb2bfc7bb1bcd9360252483af5386181
Tim Pietrusky <timpietrusky@gmail.com>
2023-04-25 22:41:06 +0200

    fix: added a secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
+	secret: process.env.SECRET,
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
