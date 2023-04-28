e0eadba00758f7bbf040225b30aed16334652460
Gregor Adams <greg@pixelass.com>
2023-04-28 18:15:43 +0200

    feat: add changelog
    seems safer

--- src/projects/fail4/constants/index.ts
+++ src/projects/fail4/constants/index.ts
@@ -1,5 +1,20 @@
 export const base = {
-	default: `
-const canvas = document.querySelector('canvas');const ctx = canvas.getContext('2d');function draw(){const FPS = 60;setTimeout(requestAnimationFrame(draw),1000/FPS)}draw();
+	default: `/** CHANGELOG
+ * v1.0.0. Set up canvas
+ */
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+/**
+ * The draw function is called every frame to update the canvas.
+ * To change the drawing logic, modify the code inside this function.
+ */
+function draw(){
+	// TODO: Add drawing logic here
+	// Set the desired FPS (frames per second) for the animation
+	const FPS = 60;
+	// Schedule the next frame to be drawn
+	setTimeout(requestAnimationFrame(draw),1000/FPS)
+}
+draw();
 	`.trim(),
 };
