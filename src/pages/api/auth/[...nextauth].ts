246414eb1930bc1857897cccc093983fbba61085
Gregor Adams <greg@pixelass.com>
2023-04-28 17:13:07 +0200

    refactor: use default secret

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,7 +5,6 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
-	secret: process.env.SECRET,
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID,
